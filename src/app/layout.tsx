0bfeb5262c7ee6aded1f039b7fe49ca54a48d9d1
dimasandriano <dimas.andriano.h@gmail.com>
2024-11-14 20:43:49 +0700

    chore(seo): update metadata and add open graph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import localFont from 'next/font/local';
 
 import '@/styles/globals.css';
 
 import { ThemeProvider } from '@/components/providers/theme.provider';
+
+import { SITE } from '@/constant/site';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -18,8 +20,52 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: 'Dimas Andriano',
-  description: 'Personal portfolio of Dimas Andriano',
+  metadataBase: new URL(SITE.url),
+  title: {
+    default: SITE.title,
+    template: `%s | ${SITE.title}`,
+  },
+  description: SITE.description,
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  openGraph: {
+    url: SITE.url,
+    title: SITE.title,
+    description: SITE.description,
+    siteName: SITE.title,
+    images: [`${SITE.url}/images/og-image.png`],
+    type: 'website',
+    locale: 'en_US',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: SITE.title,
+    description: SITE.description,
+    images: [`${SITE.url}/images/og-image.png`],
+  },
+  authors: [
+    {
+      name: SITE.url,
+      url: SITE.url,
+    },
+  ],
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
---
+++ src/constant/site.ts
@@ -0,0 +1,8 @@
+import { ENV } from '@/constant/env';
+
+export const SITE = {
+  title: 'Dimas Andriano',
+  description: 'Personal portfolio of Dimas Andriano',
+  url: ENV.BASE_URL || '',
+  image: ENV.BASE_URL + '/images/og-image.png',
+};

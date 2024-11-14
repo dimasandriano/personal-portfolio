21c8c6d60bfab69dc176d6a9beb7d377495e75b4
Dimas Andriano <dimas.andriano.h@gmail.com>
2024-11-14 16:07:08 +0700

    feat(theme): add theme provider and toggle buttons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,24 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "@/styles/globals.css";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+
+import '@/styles/globals.css';
+
+import { ThemeProvider } from '@/components/providers/theme.provider';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export const metadata: Metadata = {
-  title: "Dimas Andriano",
-  description: "Personal portfolio of Dimas Andriano",
+  title: 'Dimas Andriano',
+  description: 'Personal portfolio of Dimas Andriano',
 };
 
 export default function RootLayout({
@@ -24,11 +27,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang='en' suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+'use client';
+import { useTheme } from 'next-themes';
 import React from 'react';
 
 export default function Page() {
-  return <div></div>;
+  const { setTheme } = useTheme();
+  return (
+    <div>
+      <button onClick={() => setTheme('light')}>Light</button>
+      <button onClick={() => setTheme('dark')}>Dark</button>
+      <button onClick={() => setTheme('system')}>System</button>
+    </div>
+  );
 }
